d9b8a30c443631a4bb89a7cacfc6fd3938ddfb3d
ideyedi <gdregas18@gmail.com>
2023-02-16 16:12:23 +0900

    sensor server, client test code

--- model/sensor.go
+++ model/sensor.go
@@ -1,1 +1,201 @@
 package model
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	//"github.com/mingrammer/go-codelab/faker"
+	"faker"
+)
+
+// Sensor is common interface for any sensors
+type Sensor interface {
+	SendingOutputString() string
+	ReceivingOutputString() string
+	GenerateSensorData(epsilon float64) Sensor
+}
+
+// SensorInfo has common fields for any sensors
+type SensorInfo struct {
+	Name    string    `json:"name"`
+	Type    string    `json:"type"`
+	GenTime time.Time `json:"gen_time"`
+}
+
+// GyroSensor produces x-y-z axes angle velocity values
+type GyroSensor struct {
+	SensorInfo
+	AngleVelocityX float64 `json:"x_axis_angle_velocity"`
+	AngleVelocityY float64 `json:"y_axis_angle_velocity"`
+	AngleVelocityZ float64 `json:"z_axis_angle_velocity"`
+}
+
+// AccelSensor produces x-y-z axes gravity acceleration values
+type AccelSensor struct {
+	SensorInfo
+	GravityAccX float64 `json:"x_axis_gravity_acceleration"`
+	GravityAccY float64 `json:"y_axis_gravity_acceleration"`
+	GravityAccZ float64 `json:"z_axis_grativy_acceleration"`
+}
+
+// TempSensor produces temperature and humidity values
+type TempSensor struct {
+	SensorInfo
+	Temperature float64 `json:"temperature"`
+	Humidity    float64 `json:"humidity"`
+}
+
+func (s GyroSensor) String() string {
+	var result []string
+
+	st := fmt.Sprintf("Measured on %s", s.GenTime)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Angle Velocity of X-axis : %f", s.AngleVelocityX)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Angle Velocity of Y-axis : %f", s.AngleVelocityY)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Angle Velocity of Z-axis : %f\n", s.AngleVelocityZ)
+	result = append(result, st)
+
+	return strings.Join(result, "\n")
+}
+
+// SendingOutputString of GyroSensor create sending output string with their data
+func (s GyroSensor) SendingOutputString() string {
+	output := fmt.Sprintf("[%s] SENT : %s : %f, %f, %f",
+		s.Type, s.Name, s.AngleVelocityX, s.AngleVelocityY, s.AngleVelocityZ)
+
+	return output
+}
+
+// ReceivingOutputString of GyroSensor create receiving output string with their data
+func (s GyroSensor) ReceivingOutputString() string {
+	output := fmt.Sprintf("[%s] RECEIVED : %s : %f, %f, %f",
+		s.Type, s.Name, s.AngleVelocityX, s.AngleVelocityY, s.AngleVelocityZ)
+
+	return output
+}
+
+// GenerateSensorData generates randomly GyroSensor data
+func (s GyroSensor) GenerateSensorData(epsilon float64) Sensor {
+	gyroSensorData := GyroSensor{
+		SensorInfo: SensorInfo{
+			Name:    "GyroSensor",
+			Type:    "VelocitySensor",
+			GenTime: time.Now(),
+		},
+		AngleVelocityX: faker.GenerateAngleVelocity(epsilon),
+		AngleVelocityY: faker.GenerateAngleVelocity(epsilon),
+		AngleVelocityZ: faker.GenerateAngleVelocity(epsilon),
+	}
+
+	return gyroSensorData
+}
+
+func (s AccelSensor) String() string {
+	var result []string
+
+	st := fmt.Sprintf("Measured on %s", s.GenTime)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Gravitational Velocity of X-axis : %f", s.GravityAccX)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Gravitational Velocity of Y-axis : %f", s.GravityAccY)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Gravitational Velocity of Z-axis : %f\n", s.GravityAccZ)
+	result = append(result, st)
+
+	return strings.Join(result, "\n")
+}
+
+// SendingOutputString of AccelSensor create sending output string with their data
+func (s AccelSensor) SendingOutputString() string {
+	output := fmt.Sprintf("[%s] SENT : %s : %f, %f, %f",
+		s.Type, s.Name, s.GravityAccX, s.GravityAccY, s.GravityAccZ)
+
+	return output
+}
+
+// ReceivingOutputString of AccelSensor create receiving output string with their data
+func (s AccelSensor) ReceivingOutputString() string {
+	output := fmt.Sprintf("[%s] RECEIVED : %s : %f, %f, %f",
+		s.Type, s.Name, s.GravityAccX, s.GravityAccY, s.GravityAccZ)
+
+	return output
+}
+
+// GenerateSensorData generates randomly AccelSensor data
+func (s AccelSensor) GenerateSensorData(epsilon float64) Sensor {
+	accelSensorData := AccelSensor{
+		SensorInfo: SensorInfo{
+			Name:    "AccelerometerSensor",
+			Type:    "VelocitySensor",
+			GenTime: time.Now(),
+		},
+		GravityAccX: faker.GenerateGravityAcceleration(epsilon),
+		GravityAccY: faker.GenerateGravityAcceleration(epsilon),
+		GravityAccZ: faker.GenerateGravityAcceleration(epsilon),
+	}
+
+	return accelSensorData
+}
+
+func (s TempSensor) String() string {
+	var result []string
+
+	st := fmt.Sprintf("Measured on %s", s.GenTime)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Temperature : %f", s.Temperature)
+	result = append(result, st)
+
+	st = fmt.Sprintf("Humidity : %f\n", s.Humidity)
+	result = append(result, st)
+
+	return strings.Join(result, "\n")
+}
+
+// InlineString of TempSensor create inline string with their data
+func (s TempSensor) InlineString() string {
+	inlineStr := fmt.Sprintf("[Type : %s, Name : %s, Temp : %f, Humidity : %f]",
+		s.Type, s.Name, s.Temperature, s.Humidity)
+
+	return inlineStr
+}
+
+// SendingOutputString of TempSensor create sending output string with their data
+func (s TempSensor) SendingOutputString() string {
+	output := fmt.Sprintf("[%s] SENT : %s : %f, %f",
+		s.Type, s.Name, s.Temperature, s.Humidity)
+
+	return output
+}
+
+// ReceivingOutputString of TempSensor create receiving output string with their data
+func (s TempSensor) ReceivingOutputString() string {
+	output := fmt.Sprintf("[%s] RECEIVED : %s : %f, %f",
+		s.Type, s.Name, s.Temperature, s.Humidity)
+
+	return output
+}
+
+// GenerateSensorData generates randomly TempSensor data
+func (s TempSensor) GenerateSensorData(epsilon float64) Sensor {
+	tempSensorData := TempSensor{
+		SensorInfo: SensorInfo{
+			Name:    "TemperatureSensor",
+			Type:    "AtmosphericSensor",
+			GenTime: time.Now(),
+		},
+		Temperature: faker.GenerateTemperature(epsilon),
+		Humidity:    faker.GenerateHumidity(epsilon),
+	}
+
+	return tempSensorData
+}
---
+++ sensor_client.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	//"github.com/mingrammer/go-codelab/models"
+	"models"
+)
+
+// worker struct has necessary information to run sensor data producing job
+type worker struct {
+	ticker      *time.Ticker
+	sensor      models.Sensor
+	sensorError float64
+	serverPort  int
+}
+
+// counter is for counting the number of sending request
+type counter struct {
+	mutex sync.Mutex
+	n     int
+}
+
+func (c *counter) count() {
+	c.mutex.Lock()
+	c.n++
+	c.mutex.Unlock()
+}
+
+func (c *counter) value() int {
+	return c.n
+}
+
+// sensorWorker will be ran in goroutine to produce (random) sensor data and sends it to server
+func sensorWorker(done <-chan struct{}, w worker, c *counter) {
+	for {
+		select {
+		case <-done:
+			return
+		case <-w.ticker.C:
+			sensorData := w.sensor.GenerateSensorData(w.sensorError)
+			url := getRequestServerURL(w.serverPort)
+
+			fmt.Println(sensorData.SendingOutputString())
+
+			sendJSONSensorData(url, sensorData)
+
+			c.count()
+		}
+	}
+}
+
+func main() {
+	numCPUs := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPUs)
+
+	var sendCounter counter
+	var wg sync.WaitGroup
+	const numWorkers = 3
+
+	done := make(chan struct{})
+
+	wg.Add(numWorkers)
+
+	workerList := [numWorkers]worker{
+		worker{
+			ticker:      time.NewTicker(500 * time.Millisecond),
+			sensor:      models.GyroSensor{},
+			sensorError: 4.0,
+			serverPort:  8001,
+		},
+		worker{
+			ticker:      time.NewTicker(500 * time.Millisecond),
+			sensor:      models.AccelSensor{},
+			sensorError: 12.0,
+			serverPort:  8002,
+		},
+		worker{
+			ticker:      time.NewTicker(2 * time.Second),
+			sensor:      models.TempSensor{},
+			sensorError: 1.5,
+			serverPort:  8003,
+		},
+	}
+
+	for _, w := range workerList {
+		go func(w worker) {
+			sensorWorker(done, w, &sendCounter)
+			wg.Done()
+		}(w)
+	}
+
+	go func() {
+		for {
+			if sendCounter.value() > 100 {
+				close(done)
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Printf("\n[Count : %d] Sending is stopped.\n", sendCounter.value())
+}
+
+// getRequestServerURL just concatenate the server url and port number to get complete server url
+func getRequestServerURL(port int) string {
+	urlComponents := []string{"http://127.0.0.1", strconv.Itoa(port)}
+
+	return strings.Join(urlComponents, ":")
+}
+
+// sendJSONSensorData sends the sensor data as JSON type to server
+func sendJSONSensorData(url string, sensorValues models.Sensor) {
+	jsonBytes, err := json.Marshal(sensorValues)
+	if err != nil {
+		log.Fatal("Error occurs when mashaling the gyro sensor values")
+	}
+
+	buff := bytes.NewBuffer(jsonBytes)
+
+	resp, err := http.Post(url, "application/json", buff)
+	if err != nil || resp.StatusCode != 200 {
+		log.Fatal("Error occurs when request the post data")
+	}
+}
